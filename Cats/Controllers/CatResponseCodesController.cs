4efa7c87138c46b29ec631621887ef24dea9d59a
nasiA97 <20cat0s04@gmail.com>
2022-10-24 16:43:07 +0300

    new commit

--- Cats/Controllers/CatResponseCodesController.cs
+++ Cats/Controllers/CatResponseCodesController.cs
@@ -14,17 +14,14 @@ public class CatResponseCodesController : Controller
     {
         var result = Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
                      && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
-        if (result)
-        {
-            var request = (HttpWebRequest)WebRequest
-                .Create(url);
-            var response = (HttpWebResponse)request.GetResponse();
-            var statusCode = Convert.ToString((int)response.StatusCode);
-            var catsRequest = (HttpWebRequest)WebRequest
-                .Create($"https://http.cat/{statusCode}.jpg");
-            var catsResponse = (HttpWebResponse)catsRequest.GetResponse();
-            return Convert.ToString(catsResponse.GetResponseStream());
-        }
-        return "Fake link!";
+        if (!result) return "Fake link!";
+        var request = (HttpWebRequest)WebRequest
+            .Create(url);
+        var response = (HttpWebResponse)request.GetResponse();
+        var statusCode = Convert.ToString((int)response.StatusCode);
+        var catsRequest = (HttpWebRequest)WebRequest
+            .Create($"https://http.cat/{statusCode}.jpg");
+        var catsResponse = (HttpWebResponse)catsRequest.GetResponse();
+        return Convert.ToString(catsResponse.GetResponseStream());
     }
 }
