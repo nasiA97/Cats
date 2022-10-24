ce8ca1be8b1af1c4666e36bbceafc9f658bca8b8
nasiA97 <20cat0s04@gmail.com>
2022-10-24 16:41:40 +0300

    new commit

---
+++ Cats/Controllers/CatResponseCodesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cats.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CatResponseCodesController : Controller
+{
+    [HttpGet]
+    [Route("ProcessUrl")]
+    [Obsolete("Obsolete")]
+    public string? ProcessUrl(string url)
+    {
+        var result = Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
+                     && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        if (result)
+        {
+            var request = (HttpWebRequest)WebRequest
+                .Create(url);
+            var response = (HttpWebResponse)request.GetResponse();
+            var statusCode = Convert.ToString((int)response.StatusCode);
+            var catsRequest = (HttpWebRequest)WebRequest
+                .Create($"https://http.cat/{statusCode}.jpg");
+            var catsResponse = (HttpWebResponse)catsRequest.GetResponse();
+            return Convert.ToString(catsResponse.GetResponseStream());
+        }
+        return "Fake link!";
+    }
+}
